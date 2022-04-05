23043b5fdb30e0fc9afbb99557d1d7732ed8e40e
Dan Mann <damann1072@gmail.com>
2022-04-05 07:56:37 -0500

    Testing API

--- client/pages/Test.jsx
+++ client/pages/Test.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 let officeArr = [];
+let officialArr = [];
 
 const Test = () => {
 
-    fetch('https://www.googleapis.com/civicinfo/v2/representatives?address=4714%20Longmeadow%20Drive%20Bessemer%20AL%2035022&key=AIzaSyAt1TjpLwlV5-7Br-Kw68ZWwlI0zFswqPU')
-        .then(res => res.json())
-        .then(res => {
-            let offices = res.offices[0]
-            officeArr.push(offices);
-            console.log(offices);
-            console.log(officeArr[0].name);
+    const [details, setDetails] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/civicinfo/v2/representatives?address=716%20Richard%20Arrington%20Jr.%20Boulevard%20North%2C%20Birmingham%2C%20AL%2035203&key=AIzaSyAt1TjpLwlV5-7Br-Kw68ZWwlI0zFswqPU`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setDetails(data.officials)
         })
+        /* .then(data => {
+            let official = data.officials
+            official.forEach(element => {
+                officialArr.push(element);
+            })
+        }) */
         .catch(err => console.log(err));
+    }, []);
 
     return (
         <>
-            <div className="container text-center">
+            <div className="container">
                 <div className="row">
                     <h1>Test Page</h1>
-                    
+                </div>
+                <div id="row">
+                    <ul>
+                        <div id="row">
+                            {details.map(details => (
+                                <li key={`details-${details.name}`}>
+                                    {details.name}
+                                </li>
+                            ))}
+                        </div>
+                    </ul>
                 </div>
             </div>
         </>
