a0ae66b3cdb012459ebb1d4911648e0938426d8f
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-04-06 11:10:16 -0500

    pages added

--- client/App.jsx
+++ client/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Test from './pages/Test.jsx';
+import UsSenators from "./pages/usSenators.jsx";
+import UsReps from "./pages/usReps.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
+        <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/test" element={<Test />} />
+                <Route path="/UsSenators" element={<UsSenators />} />
+                <Route path="/UsReps" element={<UsReps />} />
             </Routes>
         </BrowserRouter>
     )
