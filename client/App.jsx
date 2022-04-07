d339c7ec6ebbf17be65311107c98e7e4fa382603
TurnerMay <turnerwmay@gmail.com>
2022-04-07 16:02:00 -0500

    Thursday Commits, new stateReps, stateSenate

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home.jsx";
 import Test from './pages/Test.jsx';
 import UsSenators from "./pages/usSenators.jsx";
 import UsReps from "./pages/usReps.jsx";
+
+import StateSenate from "./pages/stateSenate.jsx"
+import StateReps from "./pages/stateReps.jsx"
 // import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
@@ -15,6 +18,9 @@ const App = () => {
                 <Route path="/test" element={<Test />} />
                 <Route path="/UsSenators" element={<UsSenators />} />
                 <Route path="/UsReps" element={<UsReps />} />
+
+                <Route path="/StateSenate" element={<StateSenate />} />
+                <Route path="/StateReps" element={<StateReps />} />
             </Routes>
         </BrowserRouter>
     )
